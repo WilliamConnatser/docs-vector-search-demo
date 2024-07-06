5258a07fb5bd1fa39410c92237ae15339c74f876
William Connatser <me@williamconnatser.com>
2024-07-06 12:41:05 -0500

    finished workshop- added some notes

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Workshop Demo Application - Let AI Be Your Docs
 
 This is a demo application for the [Let AI Be Your Docs](https://github.com/mongodb-developer/vector-search-workshop) workshop.
+
+Run `npm run embed` which runs `./createEmbeddings.mjs`
+
+After running `npm run embed` make sure to create a MongoDB Atlas Vector Search Index using:
+
+```
+{
+  "fields": [{
+    "type": "vector",
+    "path": "embedding",
+    "numDimensions": 1536,
+    "similarity": "cosine"
+  }]
+}
+```
+
+Note: `numDimensions` is specific to the embedding model that you use so it will be different depending on what's used. `path` is the MongoDB field. `similarity`
+
+# Keeping your embeddings up to date
+
+You set up a MongoDB Trigger to automatically create new embeddings and dump them into your database.
--- createEmbeddings.mjs
+++ createEmbeddings.mjs
@@ -1,9 +1,9 @@
+import { MongoDBAtlasVectorSearch } from "@langchain/mongodb";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import "dotenv/config";
 import { promises as fsp } from "fs";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { OpenAIEmbeddings } from "@langchain/openai";
-import { MongoDBAtlasVectorSearch } from "@langchain/mongodb";
 import { MongoClient } from "mongodb";
-import "dotenv/config";
 
 const client = new MongoClient(process.env.MONGODB_ATLAS_URI || "");
 const dbName = "docs";
@@ -16,23 +16,22 @@ console.log(fileNames);
 for (const fileName of fileNames) {
   const document = await fsp.readFile(`${docs_dir}/${fileName}`, "utf8");
   console.log(`Vectorizing ${fileName}`);
-  
+
   const splitter = RecursiveCharacterTextSplitter.fromLanguage("markdown", {
+    // Chunk size can be tweaked:
+    // - The larger the chunk size, the more overhead during search
+    // - The smaller the chunk size, the less context you'll have during search
     chunkSize: 500,
     chunkOverlap: 50,
   });
   const output = await splitter.createDocuments([document]);
-  
-  await MongoDBAtlasVectorSearch.fromDocuments(
-    output,
-    new OpenAIEmbeddings(),
-    {
-      collection,
-      indexName: "default",
-      textKey: "text",
-      embeddingKey: "embedding",
-    }
-  );
+
+  await MongoDBAtlasVectorSearch.fromDocuments(output, new OpenAIEmbeddings(), {
+    collection,
+    indexName: "default",
+    textKey: "text",
+    embeddingKey: "embedding",
+  });
 }
 
 console.log("Done: Closing Connection");
